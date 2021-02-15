a95a99bdccc35a69917c66dcac7434eb224da206
Antonio Caia <antonio.caia96@gmail.com>
2021-02-15 15:18:35 +0100

    Fix <, >, >>, <> all operation working

--- notsosmart/executer.c
+++ notsosmart/executer.c
@@ -8,35 +8,6 @@ int is_builtin(char* cmd) {
 	return -1;
 }
 
-// Set up based on the io-operator the correct permission and fds
-int* get_io_fd(pn* root) {
-	int file_fd;
-	int replace_this_fd;
-
-	if (root->type == p_rdm) {
-		file_fd = open(root->args[1], O_CREAT | O_TRUNC | O_WRONLY, 0666);
-		// If a different fd is specified, set it, otherwise use the default one
-		replace_this_fd = root->args[2] == NULL ? 1 : atoi(root->args[2]);
-	}
-	else if (root->type == p_ldm) {
-		file_fd = open(root->args[1], O_RDONLY, 0666);
-		replace_this_fd = root->args[2] == NULL ? 0 : atoi(root->args[2]);
-	}
-	else if (root->type == p_lrdm) {
-		file_fd = open(root->args[1], O_CREAT | O_RDWR, 0666);
-		replace_this_fd = root->args[2] == NULL ? 0 : atoi(root->args[2]);
-	}
-	else if (root->type == p_rrdm) {
-		file_fd = open(root->args[1], O_CREAT | O_APPEND | O_WRONLY, 0666);
-		replace_this_fd = root->args[2] == NULL ? 1 : atoi(root->args[2]);
-	}
-
-	int* fds = calloc(2, sizeof(int));
-	fds[0] = file_fd;
-	fds[1] = replace_this_fd;
-	return fds;
-}
-
 // Run a standard cmd + args, no io redirection
 int execute_cmd(char** cmd_args) {
 	// Check if command is a builtin function; if true run it
@@ -136,25 +107,42 @@ int execute(pn* root) {
 	else if (is_io_ex(root->type)) {
 		int file_fd;
 		int replace_this_fd;
-		// Save stdout so we can restore it later
-		int saved_stdout = dup(STDOUT_FILENO);
-		// Get the correct fd, based on the io operator
-		int* fds = get_io_fd(root);
-		file_fd = fds[0];
-		replace_this_fd = fds[1];
+
+		if (root->type == p_rdm) {
+			file_fd = open(root->args[1], O_CREAT | O_TRUNC | O_WRONLY, 0666);
+			replace_this_fd = 1;
+		}
+		else if (root->type == p_ldm) {
+			file_fd = open(root->args[1], O_RDONLY, 0666);
+			replace_this_fd = 0;
+		}
+		else if (root->type == p_lrdm) {
+			file_fd = open(root->args[1], O_CREAT | O_RDWR, 0666);
+			replace_this_fd = 0;
+		}
+		else if (root->type == p_rrdm) {
+			file_fd = open(root->args[1], O_CREAT | O_APPEND | O_WRONLY, 0666);
+			replace_this_fd = 1;
+		}
+
 		// Handle wrong file path
-		if(file_fd == -1) {
+		if (file_fd == -1) {
 			printf("Errno: [%s]. Can't open file [%s]", strerror(errno), root->args[1]);
 			return -1;
 		}
-		// Replace stdout with the new fd
+
+		// If a different fd is specified, set it, otherwise use the default one
+		replace_this_fd = root->args[2] == NULL ? replace_this_fd : atoi(root->args[2]);
+		// Save stdout/stdin so we can restore it later
+		int save_std = dup(replace_this_fd);
+		// Switch
 		dup2(file_fd, replace_this_fd);
 		close(file_fd);
 		// Go on with program execution
 		int exec_res = execute(root->left);
-		// Restore stdout
-		dup2(saved_stdout, STDOUT_FILENO);
-		close(saved_stdout);
+		// Restore stdout/stdin
+		dup2(save_std, replace_this_fd);
+		close(save_std);
 		return exec_res;
 	}
 
